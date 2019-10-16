10ed5ab503c607894d6dc56e7fbde70f32219da9
Oskitaa <ese.betico.98@gmail.com>
2019-10-16 11:32:16 +0200

    New folder and exercises

---
+++ 1º Trimestre/Tema 3/NumerosAleatorios/src/numerosaleatorios/Ejercicios.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerosaleatorios;
+
+/**
+ *
+ * @author ese_b
+ */
+public class Ejercicios {
+
+    public void ejercicio1() {
+
+        int suma = 0, rannum;
+        for (int i = 1; i <= 30; i++) {
+
+            rannum = (int) (Math.random() * 6) + 1;
+            suma += rannum;
+            System.out.println(i + ". " + rannum);
+
+        }
+        System.out.println("La suma total de los 3 dados es " + suma);
+
+    }
+
+    public void ejercicio2() {
+    }
+
+    public void ejercicio3() {
+    }
+
+    public void ejercicio4() {
+    }
+
+    public void ejercicio5() {
+    }
+
+    public void ejercicio6() {
+    }
+
+    public void ejercicio7() {
+    }
+
+    public void ejercicio8() {
+    }
+
+    public void ejercicio9() {
+    }
+
+    public void ejercicio10() {
+    }
+
+    public void ejercicio11() {
+    }
+
+    public void ejercicio12() {
+    }
+
+    public void ejercicio13() {
+    }
+
+    public void ejercicio14() {
+    }
+
+}
