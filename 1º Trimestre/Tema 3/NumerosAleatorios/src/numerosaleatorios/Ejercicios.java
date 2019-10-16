6e3b83cb5f2af88c4cc2f6e5543c0166eda49d52
Oskitaa <ese.betico.98@gmail.com>
2019-10-16 14:59:13 +0200

    update exercises

--- 1º Trimestre/Tema 3/NumerosAleatorios/src/numerosaleatorios/Ejercicios.java
+++ 1º Trimestre/Tema 3/NumerosAleatorios/src/numerosaleatorios/Ejercicios.java
@@ -26,6 +26,54 @@ public class Ejercicios {
     }
 
     public void ejercicio2() {
+
+        String carta, palo ="";
+        int rand;
+
+        System.out.println("El programa mostrara una carta de un palo aleatoriamente.");
+
+        rand = (int) (Math.random() * 4) + 1;
+
+        switch (rand) {
+
+            case 1:
+                palo = "picas";
+                break;
+            case 2:
+                palo = "corazones";
+                break;
+            case 3:
+                palo = "diamantes";
+                break;
+            case 4:
+                palo = "treboles";
+                break;
+
+        }
+
+        rand = (int) (Math.random() * 13) + 1;
+
+        switch (rand) {
+
+            case 1:
+                carta = "As";
+                break;
+                case 11:
+                carta = "Sota";
+                break;
+                case 12:
+                carta = "Caballo";
+                break;
+                case 13:
+                carta = "Rey";
+                break;
+                default:
+                    carta = String.valueOf(rand);
+                
+        }
+        
+        System.out.println("Ha salido un "+carta+" de "+palo);
+
     }
 
     public void ejercicio3() {
